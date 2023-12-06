2aac37a6bd1ce594e615a7b83d7e4817ab60c553
gracehjy <g_huang21@yahoo.com>
2023-12-06 12:53:13 -0600

    modified touch

--- lib/mockos/TouchCommand.cpp
+++ lib/mockos/TouchCommand.cpp
@@ -1,6 +1,8 @@
 #include "mockos/TouchCommand.h"
+#include "mockos/PasswordProxy.h"
 
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 // constructor: member initialization list
@@ -14,13 +16,38 @@ TouchCommand::~TouchCommand() {}
 // tells the user how to invoke the touch command and describes the command
 void TouchCommand::displayInfo() {
     cout << "touch creates a file, touch can be invoked with the command: touch <filename>" << endl;
+    cout << "to create password protected files, touch can be invoked with the command: touch <filename> -p" << endl;
 }
 
 // asks the file factory to create a file with the provided filename
 int TouchCommand::execute(string file) {
-    AbstractFile* newFile = fileFactory->createFile(file);
+    // check if -p was used after a fileName
+    istringstream iss(file);
+    string fileName;
+    iss >> fileName;
+
+    string p;
+    iss >> p;
+
+    AbstractFile* newFile;
+
+    // if -p, create a PasswordProxy object, otherwise create a file as normally
+    if(p == "-p"){
+        string password;
+        cout << "What is the password?" << endl;
+        cin >> password;
+        newFile = new PasswordProxy(fileFactory->createFile(fileName), password);
+
+        // clear the input buffer to discard remaining characters
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    else{
+        newFile = fileFactory->createFile(fileName);
+    }
+
+    // if the file was created, add if to the filesystem. if we fail to add it, delete the file and return an error
     if(newFile){
-        int returnVal = fileSystem->addFile(file, newFile);
+        int returnVal = fileSystem->addFile(fileName, newFile);
         if(returnVal != success){
             delete newFile;
             cout << "failed to add file" << endl;
@@ -28,6 +55,8 @@ int TouchCommand::execute(string file) {
         }
         return success;
     }
+
+    // if the file was not created, return an error
     cout << "failed to create file" << endl;
     return failed_to_create_file;
 }
